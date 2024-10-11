92bfde00557737a9f96cd895bb9abc661f7f38d1
QuangVD22 <quangvd22@fpt.com>
2024-10-11 11:23:33 +0700

    Changed dependancies again

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo amazon-linux-extras install php8.3
 sudo yum update -y
-sudo yum install -y php php-mysqli httpd php-gd mysql-server
+sudo yum install -y php php-mysqli httpd php-gd
 
-# No match for aurgument to install apache2, mariadb, and libapache2-mod-php ????
+# No match for aurgument to install apache2, mariadb (also mysql), and libapache2-mod-php ???? on Amazon Linux, 
