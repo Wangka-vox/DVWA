b84ae23620b0fb229b97c6cfe910b13e92f66b37
QuangVD22 <quangvd22@fpt.com>
2024-10-15 10:28:23 +0700

    Reverting dependency and remove application stop for testing

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-isExistApp="pgrep httpd"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop apache2
-fi
+#isExistApp="pgrep httpd"
+#if [[ -n $isExistApp ]]; then
+#sudo systemctl stop apache2
+#fi
 
-isExistApp="pgrep mysql"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop mariadb.service
-fi
+#isExistApp="pgrep mysql"
+#if [[ -n $isExistApp ]]; then
+#sudo systemctl stop mariadb.service
+#fi
