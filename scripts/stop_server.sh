37999e4897f192da69e3fb12fd984761ccdef20b
QuangVD22 <quangvd22@fpt.com>
2024-10-15 10:12:15 +0700

    Reverting dependency and removed unused service

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 isExistApp="pgrep apache2"
 if [[ -n $isExistApp ]]; then
-systemctl stop apache2
+sudo systemctl stop apache2
 fi
 isExistApp=pgrep mysqld
 if [[ -n $isExistApp ]]; then
-systemctl stop mariadb.service
-fi
-isExistApp=pgrep php-fpm
-if [[ -n $isExistApp ]]; then
-systemctl stop php-fpm.service
+sudo systemctl stop mariadb.service
 fi
